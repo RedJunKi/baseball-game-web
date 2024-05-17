dfd163073a87ce023e2c304fe41eb8150285a961
redjunki <bing8821@naver.com>
2024-05-17 23:34:39 +0900

    [FEAT] 숫자 생성 크기 확인 테스트 패스 기능 구현

--- baseball-game-web/src/main/java/jk/baseballgameweb/NumberGenerator.java
+++ baseball-game-web/src/main/java/jk/baseballgameweb/NumberGenerator.java
@@ -1,6 +1,15 @@
 package jk.baseballgameweb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberGenerator {
 
-
+    public static List<Integer> generate() {
+        List<Integer> result = new ArrayList<>();
+        result.add(1);
+        result.add(2);
+        result.add(3);
+        return result;
+    }
 }
