2a96ae7a18006cd4da65474b14f3dee80399db54
redjunki <bing8821@naver.com>
2024-05-17 23:14:20 +0900

    first commit

---
+++ baseball-game-web/src/main/java/jk/baseballgameweb/BaseballGameWebApplication.java
@@ -0,0 +1,13 @@
+package jk.baseballgameweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballGameWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballGameWebApplication.class, args);
+	}
+
+}
---
+++ baseball-game-web/src/test/java/jk/baseballgameweb/BaseballGameWebApplicationTests.java
@@ -0,0 +1,13 @@
+package jk.baseballgameweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballGameWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
